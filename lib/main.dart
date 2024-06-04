bc76e08d1eafed18bef9742eab8ccce3b95d4a83
rishitha.menush <rishitha.menush@gmail.com>
2024-06-04 14:00:27 +0530

    implement stl class

--- lib/main.dart
+++ lib/main.dart
@@ -7,24 +7,31 @@ void main(){
 runApp(){
    MaterialApp(
     home: Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(colors: [
-            Color.fromARGB(255,26,2,80),
-            Color.fromARGB(255, 45, 7, 98),
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-        ),
-        child: const Center(
-          child: Text('Hello World',
-            style: TextStyle(
-            color: Colors.white,
-            fontSize: 28,
-          ),)
-        ),
-      ),
+      body: GraidentContainer(),
     ),
   );
+}
+
+class GraidentContainer extends StatelessWidget {
+  @override
+  Widget build(context){
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(colors: [
+          Color.fromARGB(255,26,2,80),
+          Color.fromARGB(255, 45, 7, 98),
+        ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+          child: Text('Hello World',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 28,
+            ),)
+      ),
+    ),
+  }
 }
